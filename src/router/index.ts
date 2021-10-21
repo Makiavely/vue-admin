0037c0f997a5aefaaa375d0c3568a9b57c1989cc
Makiavely <fiolemail@gmail.com>
2021-10-21 05:58:53 +0300

    34_Template_4-44_Add first Vue component Menu

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,16 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-
+ 
 const routes: Array<RouteRecordRaw> = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // }
+ // {
+ //   path: '/',
+ //   name: 'Home',
+ //   component: Home
+ // }
 ]
-
+ 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+ history: createWebHistory(process.env.BASE_URL),
+ routes
 })
-
+ 
 export default router
