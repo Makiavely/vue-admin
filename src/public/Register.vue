5228b061092eea1cc0c8682350422fa469ae08a3
Makiavely <fiolemail@gmail.com>
2021-11-26 06:10:17 +0300

    37_Register_Make Registration using AXIOS and Asinchronious Request and Redirect to Login page after Registration

--- src/public/Register.vue
+++ src/public/Register.vue
@@ -1,22 +1,23 @@
 <template>
   <!-- <form class="form-signin" @submit="submit"> -->
-    <form class="form-signin" @submit.prevent="submit">
+  <form class="form-signin" @submit.prevent="submit">
     <h1 class="h3 mb-3 fw-normal">Please register</h1>
 
-      <label for="first_name" class="sr-only">First Name</label>
-      <input type="text" id="first_name" class="form-control" placeholder="First Name" required v-model="firstName">
+    <label for="first_name" class="sr-only">First Name</label>
+    <input type="text" id="first_name" class="form-control" placeholder="First Name" required v-model="firstName">
 
-      <label for="last_name" class="sr-only">Last Name</label>
-      <input type="text" id="last_name" class="form-control" placeholder="Last Name" required v-model="lastName">
+    <label for="last_name" class="sr-only">Last Name</label>
+    <input type="text" id="last_name" class="form-control" placeholder="Last Name" required v-model="lastName">
 
-      <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="text" id="inputEmail" class="form-control" placeholder="Email address" required v-model="email">
+    <label for="inputEmail" class="sr-only">Email address</label>
+    <input type="text" id="inputEmail" class="form-control" placeholder="Email address" required v-model="email">
 
-      <label for="inputPassword" class="sr-only">Password</label>
-      <input type="text" id="inputPassword" class="form-control" placeholder="Password" required v-model="password">
+    <label for="inputPassword" class="sr-only">Password</label>
+    <input type="text" id="inputPassword" class="form-control" placeholder="Password" required v-model="password">
 
-      <label for="passwordConfirm" class="sr-only">Password Confirm</label>
-      <input type="text" id="passwordConfirm" class="form-control" placeholder="Password Confirm" required v-model="passwordConfirm">
+    <label for="passwordConfirm" class="sr-only">Password Confirm</label>
+    <input type="text" id="passwordConfirm" class="form-control" placeholder="Password Confirm" required
+           v-model="passwordConfirm">
 
     <button class="w-100 btn btn-lg btn-primary" type="submit">Register</button>
   </form>
@@ -24,40 +25,62 @@
 
 <script>
 import {ref} from 'vue';
+import axios from 'axios';
+import {useRouter} from "vue-router";
 
-  export default {    
-    name: "Register",
-    setup() {
-      const firstName = ref('');
-      const lastName = ref('');
-      const email = ref('');
-      const password = ref('');
-      const passwordConfirm = ref('');
+export default {
+  name: "Register",
+  setup() {
+    const firstName = ref('');
+    const lastName = ref('');
+    const email = ref('');
+    const password = ref('');
+    const passwordConfirm = ref('');
+    const router = useRouter();
 
-      const submit = () => {
-        console.log({
-          first_name: firstName.value,
-          last_name: lastName.value,
-          email: email.value,
-          password: password.value,
-          password_confirm: passwordConfirm.value,
-        })
-      }
+    /*     const submit = () => {
+           axios.post('http://localhost:8000/api/register',{
+             first_name: firstName.value,
+             last_name: lastName.value,
+             email: email.value,
+             password: password.value,
+             password_confirm: passwordConfirm.value,
+           }).then(
+             res => {
+               console.log(res)
+             }
+           )
+         }*/
 
-      return {
-        firstName: firstName,
-        lastName,
-        email,
-        password,
-        passwordConfirm,
-        submit
-      }
+
+    const submit = async () => {
+      /*const response = await axios.post('http://localhost:8000/api/register',{*/
+      await axios.post('http://localhost:8000/api/register', {
+        first_name: firstName.value,
+        last_name: lastName.value,
+        email: email.value,
+        password: password.value,
+        password_confirm: passwordConfirm.value,
+      });
+
+      /* console.log(response);*/
+      await router.push('/login');
     }
- }
+
+    return {
+      firstName: firstName,
+      lastName,
+      email,
+      password,
+      passwordConfirm,
+      submit
+    }
+  }
+}
 </script>
 
 <style scoped>
-  html,
+html,
 body {
   height: 100%;
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,15 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Register from '@/public/Register.vue'
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+import Register from '@/public/Register.vue';
+import Login from '@/public/Login.vue';
 
 const routes: Array<RouteRecordRaw> = [
- {
-   path: '/register',
-   component: Register
- }
+    {path: '/register', component: Register},
+    {path: '/login', component: Login},
 ]
- 
+
 const router = createRouter({
- history: createWebHistory(process.env.BASE_URL),
- routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
- 
+
 export default router
