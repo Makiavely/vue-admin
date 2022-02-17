28f1a68df167d8c02f37101010b71a1ef897c81e
Makiavely <fiolemail@gmail.com>
2022-02-17 22:23:21 +0300

    61f_Chart_Making chart on Dashboard page

--- src/secure/dashboard/Dashboard.vue
+++ src/secure/dashboard/Dashboard.vue
@@ -1,32 +1,58 @@
 <template>
-<!--  <h2>Section title</h2>
-  <div class="table-responsive">
-    <table class="table table-striped table-sm">
-      <thead>
-      <tr>
-        <th>#</th>
-        <th>Header</th>
-        <th>Header</th>
-        <th>Header</th>
-        <th>Header</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr>
-        <td>1,001</td>
-        <td>Lorem</td>
-        <td>ipsum</td>
-        <td>dolor</td>
-        <td>sit</td>
-      </tr>
-      </tbody>
-    </table>
-  </div>-->
-  Dashboard
+  <h2>Daily Sales</h2>
+
+  <div id="chart"></div>
 </template>
 
 <script>
+import {onMounted} from 'vue';
+import axios from 'axios';
+import * as c3 from 'c3';
+
 export default {
-name: "Dashboard"
+  name: "Dashboard",
+  setup() {
+    onMounted(async () => {
+      const chart = c3.generate({
+
+        bindto: '#chart',
+        data: {
+          x: 'x',
+          columns: [
+
+            /*['x'],
+            ['Sales']*/
+
+            /*['x', 2],
+            ['Sales', 30]*/
+
+            ['x'],
+            ['Sales']
+          ],
+          types: {
+            Sales: 'bar'
+          }
+        },
+        axis: {
+          x: {
+            type: 'timeseries',
+            tick: {
+              format: '%Y-%m-%d'
+            }
+          }
+        }
+      });
+      const response = await axios.get('chart');
+
+      const records = response.data.data;
+
+      chart.load({
+        columns: [
+          ['x', ...records.map(r => r.date)],
+          ['Sales', ...records.map(r => r.sum)]
+        ]
+      });
+    });
+  }
 }
 </script>
